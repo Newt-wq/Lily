5380fedf0438153a9274e74b830d26e139247056
Diena <diena@example.com>
2026-08-13 21:24:49 +0700

    bypass countdown target date completely

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,5 @@
-// Target date: 14 August 2026 00:00 WIB (GMT+7)
-// 14 August 2026 00:00 WIB is equivalent to 13 August 2026 17:00 UTC
-export const TARGET_DATE = new Date('2026-08-13T17:00:00Z');
+// Target date bypassed - expired in past so timer is finished
+export const TARGET_DATE = new Date('2020-01-01T00:00:00Z');
 
 export const PIN = '21140503';
 export const PIN_HINT = 'Tanggal lahir kamu 🤍';
